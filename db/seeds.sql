1244d249d02d20f6f29de26ea06a026854005418
jbarry89 <joelbarry8907@hotmail.com>
2025-01-05 00:49:10 -0500

    Added more pre-populated data in the database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,35 @@
--- Go into the employee_db Database
-\c employees_db
-
 -- Insert data into Department Table
 INSERT INTO department (name)
 VALUES ('Engineering'),
        ('Sales'),
        ('Human Resources'),
-       ('Marketing');
+       ('Marketing'),
+       ('Production'),
+       ('Shipping');
 
 
 -- Insert data into Role Table
 INSERT INTO role (title, salary, department_id)
-VALUES ('Software Engineer', 90000, 1),
-       ('Sales Representative'60000, 2),
-       ('Human Manager', 70000, 3),
-       ('Marketing Coordinator', 55000, 4);
+VALUES ('Software Engineer', 115000, 1),
+       ('Sales Representative', 60000, 2),
+       ('Human Resource Manager', 75000, 3),
+       ('Marketing Coordinator', 55000, 4),
+       ('Production Manager', 85000, 5),
+       ('Shipping Clerk', 44000, 6),
+       ('Recruiter', 50000, 3);
 
 -- Insert data into Employee Table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Joseph', 'Smith', 1, 1),
-       ('Jane', 'Lacey',2, 2),
-       ('Susan', 'Clark', 4, NULL),
+VALUES ('Joseph', 'Smith', 1, 5),
+       ('Jane', 'Lacey',2, 5),
+       ('Susan', 'Clark', 7, 4),
        ('Emily', 'James', 3, NULL),
-       ('David', 'Wilson', 1, 1);
+       ('David', 'Wilson', 1, 5),
+       ('Nathan', 'Howard', 5, NULL),
+       ('Megan', 'Fuller', 4, 5),
+       ('Robert', 'Cummings', 6, 5);
+       
+
+-- SELECT * FROM department;
+-- SELECT * FROM role;
+-- SELECT * FROM employee;
